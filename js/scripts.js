fe116d2fc7e07ab1979ac0cc2b6fd408b25946fc
Kyle Hubbard <kyle.james.hubbard@gmail.com>
2020-06-05 10:33:59 -0700

    add first two questions to index.html

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// Business Logic
+let number = null;
+
+function check(number) {
+  let score = null;
+}
+
+// User Interface Logic
+
+$(document).ready(function() {
+  $("form#question1").submit(function(event) {
+    const answer = $()
+  });
+});
